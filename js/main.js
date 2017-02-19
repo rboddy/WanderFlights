52b5622d8764b507006eaa3eec88d655a8ad2465
Ryan Boddy <avidryanb@gmail.com>
2017-02-18 23:10:44 -0500

    Wrote an event listener for enroll that should only work if the passwords in password and confirm password match. Not entirely sure why it isn't working, could have a lot to do with the hotel internet, going to do one last test

--- js/main.js
+++ js/main.js
@@ -14,10 +14,14 @@
     
     const emailTxt = document.getElementById('emailTxt');
     const passwordTxt = document.getElementById('passwordTxt');
+    const cPassword = document.getElementById('cPassword');
     const loginBtn = document.getElementById('loginButton');
     const enrollBtn = document.getElementById('enrollButton');
+    const enrollSubmit = document.getElementById('enrollSubmit');
     
     //add listeners:
+    
+    //Login in Listener
     
     loginBtn.addEventListener('click', e => {
         //get email and pass
@@ -28,6 +32,39 @@
         
         const promise = auth.signInWithEmailAndPassword(email, password);
         promise.catch(e => console.log(e.message));
+    });
+    
+    //Enroll Listener
+    
+    submitBtn.addEventListener('click', e => {
+        
+    if (passwordTxt.value == cPassword.value) {
+        
+        //get email and pass
+        //TODO: Check for real email
+        const email = emailTxt.value;
+        const password = passwordTxt.value;
+        const auth = firebase.auth();
+        //Enroll
+        
+        const promise = auth.createUserWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+        
+    }
+    else {
+        window.alert("Passwords do not match, please retype them.");
+    }
+        
+    });
+    
+    //add realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser){
+            console.log(firebaseUser);
+        }
+        else {
+            console.log("Not Logged In");
+        }
     });
 
 }());
